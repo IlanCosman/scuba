70fc1ab928dff58f38352d20e5ae76a46a79d37c
Ilan Cosman <ilancosman@gmail.com>
2020-10-06 09:52:11 -0700

    Make install a function instead of a script

--- tools/install.fish
+++
@@ -1,7 +0,0 @@
-set -l location /tmp/scuba
-
-rm -rf $location
-git clone --quiet --depth=1 https://github.com/IlanCosman/scuba $location
-cp -r $location/{completions,conf.d,functions} $__fish_config_dir 2>/dev/null
-
-exec fish
