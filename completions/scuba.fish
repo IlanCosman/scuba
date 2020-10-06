1f306556912034c8d6ed1f81e9ac84bf6c728dec
Ilan Cosman <ilancosman@gmail.com>
2020-10-05 21:59:19 -0700

    Add bug-report command

--- completions/scuba.fish
+++ completions/scuba.fish
@@ -1,8 +1,9 @@
+complete -c scuba -x -n __fish_use_subcommand -a --help -d "Print help message"
+complete -c scuba -x -n __fish_use_subcommand -a --version -d "Print scuba version"
+complete -c scuba -x -n __fish_use_subcommand -a bug-report -d "Print info for use in bug reports"
 complete -c scuba -x -n __fish_use_subcommand -a install -d "Install plugins"
 complete -c scuba -x -n __fish_use_subcommand -a list -d "List installed plugins matching regex"
 complete -c scuba -x -n __fish_use_subcommand -a remove -d "Remove plugins"
-complete -c scuba -x -n __fish_use_subcommand -a --help -d "Print help message"
-complete -c scuba -x -n __fish_use_subcommand -a --version -d "Print scuba version"
 
 for plugin in $_scuba_plugins
     complete -c scuba -x -n "__fish_seen_subcommand_from remove" -a $plugin
---
+++ functions/_scuba_sub_bug-report.fish
@@ -0,0 +1,5 @@
+function _scuba_sub_bug-report
+    fish --version
+    scuba --version
+    printf '%s\n' $TERM
+end
