ba2356d4aab6822c6e85a2f683a0cc92a1149374
Ilan Cosman <ilancosman@gmail.com>
2020-10-06 13:21:39 -0700

    Install accepts branch, tag, sha

--- functions/_scuba_sub_install.fish
+++ functions/_scuba_sub_install.fish
@@ -1,17 +1,19 @@
 function _scuba_sub_install
-    for name in $argv
-        printf '%s\n' "Installing $name..."
+    for arg in $argv
+        printf '%s\n' "Installing $arg..."
 
-        set -l nameEscaped (string escape --style=var $name)
-        set -l location /tmp/scuba/$nameEscaped
+        set -l argSplit (string split --right --max=1 '@' $arg)
+
+        set -l argEscaped (string escape --style=var $arg)
+        set -l location /tmp/scuba/$argEscaped
 
         rm -rf $location
 
-        if test -e $name
-            cp -r $name $location
-        else if git clone --quiet --depth=1 https://github.com/$name $location
+        if test -e $arg
+            cp -r $arg $location
+        else if git clone --quiet https://github.com/$argSplit[1] $location && git -C $location checkout --quiet $argSplit[2]
         else
-            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $name" \n
+            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $arg" \n
             continue
         end
 
@@ -19,18 +21,18 @@ function _scuba_sub_install
             mkdir -p $location/functions
             cp $location/*.fish $location/functions # copy them into location's function directory
         end
-        cp -r $location/{completions,conf.d,functions} $__fish_config_dir 2>/dev/null # Don't error if any of the directories don't exist
+        cp -r $location/{completions,conf.d,functions} $__fish_config_dir 2>/dev/null # Don't error if any directory doesn't exist
 
-        set -U _scuba_"$nameEscaped"_files (string replace $location '' $location/{completions,conf.d,functions}/**)
+        set -U _scuba_"$argEscaped"_files (string replace $location '' $location/{completions,conf.d,functions}/**)
 
-        if not contains $name $_scuba_plugins
-            set -Ua _scuba_plugins $name
+        if not contains $arg $_scuba_plugins
+            set -Ua _scuba_plugins $arg
         end
 
         source $location/install.fish 2>/dev/null # Don't error if install.fish doesn't exist
 
         set_color --bold blue
-        printf '%s\n' "$name installed!"
+        printf '%s\n' "$arg installed!"
         set_color normal
     end
 
--- functions/_scuba_sub_remove.fish
+++ functions/_scuba_sub_remove.fish
@@ -1,24 +1,24 @@
 function _scuba_sub_remove
-    for name in $argv
-        if not contains $name $_scuba_plugins
-            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $name" \n
+    for arg in $argv
+        printf '%s\n' "Removing $arg..."
+
+        if not contains $arg $_scuba_plugins
+            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $arg" \n
             continue
         end
 
-        printf '%s\n' "Removing $name..."
-
-        set -l nameEscaped (string escape --style=var $name)
-        set -l fileVarName _scuba_"$nameEscaped"_files
+        set -l argEscaped (string escape --style=var $arg)
+        set -l fileVararg _scuba_"$argEscaped"_files
 
         # Use -r to remove any custom directories
         # Ignore errors, as some files may be contained within directories
-        rm -r $__fish_config_dir/$$fileVarName 2>/dev/null
+        rm -r $__fish_config_dir/$$fileVararg 2>/dev/null
 
-        set -e $fileVarName
-        set -e _scuba_plugins[(contains --index $name $_scuba_plugins)]
+        set -e $fileVararg
+        set -e _scuba_plugins[(contains --index $arg $_scuba_plugins)]
 
         set_color --bold blue
-        printf '%s\n' "$name removed!"
+        printf '%s\n' "$arg removed!"
         set_color normal
     end
 
