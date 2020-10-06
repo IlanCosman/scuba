5028ce82a1586b837dc517ecc12d76b83ae34150
Ilan Cosman <ilancosman@gmail.com>
2020-10-06 10:28:31 -0700

    Just use _scuba_sub_install to install scuba

--- tools/scuba_install.fish
+++
@@ -1,9 +0,0 @@
-function scuba_install
-    set -l location /tmp/scuba
-
-    rm -rf $location
-    git clone --quiet --depth=1 https://github.com/IlanCosman/scuba $location
-    cp -r $location/{completions,conf.d,functions} $__fish_config_dir 2>/dev/null
-
-    fish --init-command="scuba install IlanCosman/scuba"
-end
