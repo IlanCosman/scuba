2d33878a6a3afa9fb3b676df9ed1dec47d838d08
Ilan Cosman <ilancosman@gmail.com>
2020-10-06 11:14:07 -0700

    install and remove accept multiple arguments now

--- functions/_scuba_sub_install.fish
+++ functions/_scuba_sub_install.fish
@@ -1,36 +1,38 @@
-function _scuba_sub_install -a name
-    printf '%s\n' "Installing $name..."
+function _scuba_sub_install
+    for name in $argv
+        printf '%s\n' "Installing $name..."
 
-    set -l nameEscaped (string escape --style=var $name)
-    set -l location /tmp/scuba/$nameEscaped
+        set -l nameEscaped (string escape --style=var $name)
+        set -l location /tmp/scuba/$nameEscaped
 
-    rm -rf $location
+        rm -rf $location
 
-    if test -e $name
-        cp -r $name $location
-    else if git clone --quiet --depth=1 https://github.com/$name $location
-    else
-        printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $name" \n
-        return 1
+        if test -e $name
+            cp -r $name $location
+        else if git clone --quiet --depth=1 https://github.com/$name $location
+        else
+            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $name" \n
+            continue
+        end
+
+        if string match --quiet --regex "\.fish\$" $location/* # If there are any top level fish files
+            mkdir -p $location/functions
+            cp $location/*.fish $location/functions # copy them into location's function directory
+        end
+        cp -r $location/{completions,conf.d,functions} $__fish_config_dir 2>/dev/null # Don't error if any of the directories don't exist
+
+        set -U _scuba_"$nameEscaped"_files (string replace $location '' $location/{completions,conf.d,functions}/**)
+
+        if not contains $name $_scuba_plugins
+            set -Ua _scuba_plugins $name
+        end
+
+        source $location/install.fish 2>/dev/null # Don't error if install.fish doesn't exist
+
+        set_color --bold blue
+        printf '%s\n' "$name installed!"
+        set_color normal
     end
-
-    if string match --quiet --regex "\.fish\$" $location/* # If there are any top level fish files
-        mkdir -p $location/functions
-        cp $location/*.fish $location/functions # copy them into location's function directory
-    end
-    cp -r $location/{completions,conf.d,functions} $__fish_config_dir 2>/dev/null # Don't error if any of the directories don't exist
-
-    set -U _scuba_"$nameEscaped"_files (string replace $location '' $location/{completions,conf.d,functions}/**)
-
-    if not contains $name $_scuba_plugins
-        set -Ua _scuba_plugins $name
-    end
-
-    source $location/install.fish 2>/dev/null # Don't error if install.fish doesn't exist
-
-    set_color --bold blue
-    printf '%s\n' "$name installed!"
-    set_color normal
 
     exec fish
 end
--- functions/_scuba_sub_remove.fish
+++ functions/_scuba_sub_remove.fish
@@ -1,24 +1,26 @@
-function _scuba_sub_remove -a name
-    if not contains $name $_scuba_plugins
-        printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $name" \n
-        return 1
+function _scuba_sub_remove
+    for name in $argv
+        if not contains $name $_scuba_plugins
+            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $name" \n
+            continue
+        end
+
+        printf '%s\n' "Removing $name..."
+
+        set -l nameEscaped (string escape --style=var $name)
+        set -l fileVarName _scuba_"$nameEscaped"_files
+
+        # Use -r to remove any custom directories
+        # Ignore errors, as some files may be contained within directories
+        rm -r $__fish_config_dir/$$fileVarName 2>/dev/null
+
+        set -e $fileVarName
+        set -e _scuba_plugins[(contains --index $name $_scuba_plugins)]
+
+        set_color --bold blue
+        printf '%s\n' "$name removed!"
+        set_color normal
     end
-
-    printf '%s\n' "Removing $name..."
-
-    set -l nameEscaped (string escape --style=var $name)
-    set -l fileVarName _scuba_"$nameEscaped"_files
-
-    # Use -r to remove any custom directories
-    # Ignore errors, as some files may be contained within directories
-    rm -r $__fish_config_dir/$$fileVarName 2>/dev/null
-
-    set -e $fileVarName
-    set -e _scuba_plugins[(contains --index $name $_scuba_plugins)]
-
-    set_color --bold blue
-    printf '%s\n' "$name removed!"
-    set_color normal
 
     exec fish
 end
