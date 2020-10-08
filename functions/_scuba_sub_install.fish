34c7f3d739e1cb115522338ad22ef6ea77e78df5
Ilan Cosman <ilancosman@gmail.com>
2020-10-08 16:23:24 -0700

    Minor edits

--- functions/_scuba_sub_install.fish
+++ functions/_scuba_sub_install.fish
@@ -1,54 +1,56 @@
 function _scuba_sub_install
-    if set -q argv[1]
-        set -l arg $argv[1]
+    if not set -q argv[1]
+        return
+    end
 
-        printf '%s\n' "Installing $arg..."
+    set -l arg $argv[1]
 
-        set -l argSplit (string split --right --max=1 '@' $arg)
+    printf '%s\n' "Installing $arg..."
 
-        set -l argEscaped (string escape --style=var $arg)
-        set -l location /tmp/scuba/$argEscaped
+    set -l argSplit (string split --right --max=1 '@' $arg)
 
-        rm -rf $location
+    set -l argEscaped (string escape --style=var $arg)
+    set -l location /tmp/scuba/$argEscaped
 
-        if test -e $argSplit[1]
-            cp -r $argSplit[1] $location
-        else if GIT_TERMINAL_PROMPT=0 git clone https://github.com/$argSplit[1] $location
-        else
-            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $argSplit[1]" \n
-            exec fish --init-command="set -g fish_greeting; _scuba_sub_install $argv[2..-1]"
-        end
+    rm -rf $location
 
-        if set -q argSplit[2] && not git -c advice.detachedHead=false -C $location checkout $argSplit[2]
-            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $argSplit[2]" \n
-            exec fish --init-command="set -g fish_greeting; _scuba_sub_install $argv[2..-1]"
-        end
-
-        set -U _scuba_"$argEscaped"_sha (git -C $location rev-parse HEAD 2>/dev/null)
-
-        if not contains $arg $_scuba_plugins
-            set -Ua _scuba_plugins $arg
-        end
-
-        mkdir -p $location/{completions,conf.d,functions}
-
-        if string match --quiet --regex "\.fish\$" $location/* # If there are any top level fish files
-            cp $location/*.fish $location/functions # copy them into location's function directory
-        end
-
-        cp -r $location/{completions,conf.d,functions} $__fish_config_dir
-
-        set -U _scuba_"$argEscaped"_files (string replace $location '' $location/{completions,conf.d,functions}/**)
-        set -l fileVarName _scuba_"$argEscaped"_files
-
-        for file in (basename -s .fish $$fileVarName)
-            emit "$file"_install
-        end
-
-        set_color --bold blue
-        printf '%s\n' "$arg installed!"
-        set_color normal
-
+    if test -e $argSplit[1]
+        cp -r $argSplit[1] $location
+    else if GIT_TERMINAL_PROMPT=0 git clone https://github.com/$argSplit[1] $location
+    else
+        printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $argSplit[1]" \n
         exec fish --init-command="set -g fish_greeting; _scuba_sub_install $argv[2..-1]"
     end
+
+    if set -q argSplit[2] && not git -c advice.detachedHead=false -C $location checkout $argSplit[2]
+        printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $argSplit[2]" \n
+        exec fish --init-command="set -g fish_greeting; _scuba_sub_install $argv[2..-1]"
+    end
+
+    set -U _scuba_"$argEscaped"_sha (git -C $location rev-parse HEAD 2>/dev/null)
+
+    if not contains $arg $_scuba_plugins
+        set -Ua _scuba_plugins $arg
+    end
+
+    mkdir -p $location/{completions,conf.d,functions}
+
+    if string match --quiet --regex "\.fish\$" $location/* # If there are any top level fish files
+        cp $location/*.fish $location/functions # copy them into location's function directory
+    end
+
+    cp -r $location/{completions,conf.d,functions} $__fish_config_dir
+
+    set -U _scuba_"$argEscaped"_files (string replace $location '' $location/{completions,conf.d,functions}/**)
+    set -l fileVarName _scuba_"$argEscaped"_files
+
+    for file in (basename -s .fish $$fileVarName)
+        emit "$file"_install
+    end
+
+    set_color --bold blue
+    printf '%s\n' "$arg installed!"
+    set_color normal
+
+    exec fish --init-command="set -g fish_greeting; _scuba_sub_install $argv[2..-1]"
 end
--- functions/_scuba_sub_remove.fish
+++ functions/_scuba_sub_remove.fish
@@ -1,30 +1,32 @@
 function _scuba_sub_remove
-    if set -q argv[1]
-        set -l arg $argv[1]
+    if not set -q argv[1]
+        return
+    end
 
-        printf '%s\n' "Removing $arg..."
+    set -l arg $argv[1]
 
-        if set -e _scuba_plugins[(contains --index $arg $_scuba_plugins)]
-            set -l argEscaped (string escape --style=var $arg)
-            set -l fileVarName _scuba_"$argEscaped"_files
+    printf '%s\n' "Removing $arg..."
 
-            # Use -r to remove any custom directories
-            # Ignore errors, as some files may be contained within directories
-            rm -r $__fish_config_dir/$$fileVarName 2>/dev/null
+    if set -e _scuba_plugins[(contains --index $arg $_scuba_plugins)]
+        set -l argEscaped (string escape --style=var $arg)
+        set -l fileVarName _scuba_"$argEscaped"_files
 
-            for file in (basename -s .fish $$fileVarName)
-                emit "$file"_uninstall
-            end
+        # Use -r to remove any custom directories
+        # Ignore errors, as some files may be contained within directories
+        rm -r $__fish_config_dir/$$fileVarName 2>/dev/null
 
-            set -e _scuba_"$argEscaped"_files
-
-            set_color --bold blue
-            printf '%s\n' "$arg removed!"
-            set_color normal
-        else
-            printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $arg" \n
+        for file in (basename -s .fish $$fileVarName)
+            emit "$file"_uninstall
         end
 
-        exec fish --init-command="set -g fish_greeting; _scuba_sub_remove $argv[2..-1]"
+        set -e _scuba_"$argEscaped"_files
+
+        set_color --bold blue
+        printf '%s\n' "$arg removed!"
+        set_color normal
+    else
+        printf '%s' (set_color --bold red) "error: " (set_color normal) "target not found: $arg" \n
     end
+
+    exec fish --init-command="set -g fish_greeting; _scuba_sub_remove $argv[2..-1]"
 end
