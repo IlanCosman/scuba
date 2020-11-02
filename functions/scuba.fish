3c6a42943590b3a1ef68940e031bec11a08255c1
Ilan Cosman <ilancosman@gmail.com>
2020-11-01 16:23:25 -0800

    Simplify and improve help message

--- functions/scuba.fish
+++ functions/scuba.fish
@@ -4,31 +4,22 @@ function scuba
     if set -q _flag_version
         printf '%s\n' 'scuba, version 1.0.0'
     else if set -q _flag_help
-        _scuba_help
+        printf '%s\n' \
+            "Usage: scuba [option] subcommand" \
+            "" \
+            "Options:" \
+            "  -v or --version  print scuba version" \
+            "  -h or --help     print this help message" \
+            "" \
+            "Subcommands:" \
+            "  install <plugin(s)>   install plugins" \
+            "  remove  <plugin(s)>   remove installed plugins" \
+            "  update  [plugin(s)]   update installed plugins" \
+            "  list    <regex>       list installed plugins matching regex"
     else if functions --query _scuba_sub_$argv[1]
         _scuba_sub_$argv[1] $argv[2..-1]
     else
-        _scuba_help
+        printf '%s' $_scuba_error 'no operation specified (use -h for help)' \n
         return 1
     end
-end
-
-function _scuba_help
-    set -l cmd (set_color $fish_color_command)
-    set -l param (set_color $fish_color_param)
-    set -l b (set_color --bold)
-    set -l n (set_color normal)
-
-    printf '%s\n' \
-        "Usage:$cmd scuba$n [options]$param subcommand$n" \
-        "" \
-        "Options:" \
-        "  -v or --version  print scuba version" \
-        "  -h or --help     print this help message" \
-        "" \
-        "Subcommands:" \
-        $b"  install$n <plugins...>  install plugins" \
-        $b"  remove$n <plugins...>   remove installed plugins" \
-        $b"  update$n <plugins...>   update installed plugins" \
-        $b"  list$n                  list installed plugins matching regex"
 end
